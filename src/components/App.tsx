5718dedc996212bfc9449fe785cf0985cd5ecf2f
Vaibhav Mule <vaibhavmule135@gmail.com>
2025-12-09 11:43:08 +0530

    Remove CeloRewardModal import and usage from App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,6 @@ import { sdk } from "@farcaster/miniapp-sdk";
 import { BottomNav, type TabType } from "~/components/ui/BottomNav";
 import { ScoreTab, ImproveTab } from "~/components/ui/tabs";
 import { AddAppPrompt } from "~/components/ui/AddAppPrompt";
-import { CeloRewardModal } from "~/components/ui/CeloRewardModal";
 import { useNeynarUser } from "../hooks/useNeynarUser";
 
 export interface AppProps {
@@ -181,9 +180,6 @@ export default function App(
   // --- Render ---
   return (
     <>
-      {/* Celo reward popup - shows on first visit */}
-      <CeloRewardModal />
-
       {/* Auto-trigger "Add to App" 1 second after score is checked */}
       <AddAppPrompt hasScore={neynarUser !== null} delay={1000} />
 
